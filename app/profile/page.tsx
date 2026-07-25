5c6ac0101840a7e5c763567ecc7d86c70432d4bf
mahzadkhosraviani <khosravianimahzad@gmail.com>
2026-07-25 15:54:11 +0330

    adding the logic of update section for the users info

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import { GetFetch } from "../utils/fetch";
+import EditForm from "@/components/profile/info/EditForm";
 
 export default async function profilePage() {
   const cookieStore = await cookies();
@@ -11,36 +12,7 @@ export default async function profilePage() {
 
   return (
     <div className="vh-70">
-      <div className="row g-4">
-        <div className="col col-md-6">
-          <label className="form-label">نام و نام خانوادگی</label>
-          <input
-            type="text"
-            className="form-control"
-            defaultValue={user.name}
-          />
-        </div>
-        <div className="col col-md-6">
-          <label className="form-label">ایمیل</label>
-          <input
-            type="text"
-            className="form-control"
-            defaultValue={user.email}
-          />
-        </div>
-        <div className="col col-md-6">
-          <label className="form-label">شماره تلفن</label>
-          <input
-            type="text"
-            disabled
-            className="form-control"
-            defaultValue={user.cellphone}
-          />
-        </div>
-      </div>
-      <button type="submit" className="btn btn-primary mt-4">
-        ویرایش
-      </button>
+      <EditForm user={user} />
     </div>
   );
 }
