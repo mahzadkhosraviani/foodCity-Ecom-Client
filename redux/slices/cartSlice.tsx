f18d5071be5cf7f99d35d07c2eb14c40d701b6a1
mahzadkhosraviani <khosravianimahzad@gmail.com>
2026-07-31 16:04:55 +0330

    creating the shopping cart page

--- redux/slices/cartSlice.tsx
+++ redux/slices/cartSlice.tsx
@@ -15,7 +15,21 @@ export const cartSlice = createSlice({
     removeFromCart: (state, action) => {
       state.cart = state.cart.filter((p) => p.id !== action.payload);
     },
+    increment: (state, action) => {
+      state.cart = state.cart.map((p) =>
+        p.id === action.payload ? { ...p, qty: p.qty + 1 } : p,
+      );
+    },
+    decrement: (state, action) => {
+      state.cart = state.cart.map((p) =>
+        p.id === action.payload ? { ...p, qty: p.qty - 1 } : p,
+      );
+    },
+    clearCart:(state)=>{
+      state.cart=[]
+    }
   },
 });
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, increment, decrement,clearCart } =
+  cartSlice.actions;
 export const cartReducer = cartSlice.reducer;
